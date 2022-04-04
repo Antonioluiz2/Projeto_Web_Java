01fb3873d7de4679c47d1fc770efc5c552e8994b
Luiz <antonioluizti@hotmail.com>
2022-04-04 07:51:00 -0300

    li

--- src/main/java/com/luiz/projectWeb/controller/EmpregoController.java
+++ src/main/java/com/luiz/projectWeb/controller/EmpregoController.java
@@ -1,11 +1,12 @@
 package com.luiz.projectWeb.controller;
 
-import javax.validation.Valid;
+//import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,7 +30,7 @@ public class EmpregoController {
 		return "empregoForm";
 	}
 	@PostMapping("/process")
-	public String processForm(@Valid Emprego emprego, BindingResult result) {
+	public String processForm(@Validated Emprego emprego, BindingResult result) {
 		if(result.hasErrors()) {
 			return "empregoForm";
 			}
